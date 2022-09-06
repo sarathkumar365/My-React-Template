42980e31381aeb899e440c363a2dfc6994240fbb
sarath kumar <sarathkumar@pop-os.localdomain>
2022-09-06 20:58:01 +0530

    added all files and configs

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="home">
+        <h1>hello world 🙂 </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
